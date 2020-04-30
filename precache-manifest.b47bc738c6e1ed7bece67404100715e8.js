0076f9244737b5e698b78ac93628e308c7c5355d
Enamul Haque <enamul.iu91@gmail.com>
2020-05-01 00:25:43 +0600

    Updates

---
+++ precache-manifest.b47bc738c6e1ed7bece67404100715e8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "798ae0a002214379bfd0665be16021a9",
+    "url": "/monosters/index.html"
+  },
+  {
+    "revision": "41cb25b71e000f0c1d73",
+    "url": "/monosters/static/css/main.e73aa96d.chunk.css"
+  },
+  {
+    "revision": "50178188a875b58681d5",
+    "url": "/monosters/static/js/2.e9265ccf.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monosters/static/js/2.e9265ccf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "41cb25b71e000f0c1d73",
+    "url": "/monosters/static/js/main.1253a206.chunk.js"
+  },
+  {
+    "revision": "3e34c1d98f2313f90d58",
+    "url": "/monosters/static/js/runtime-main.a9ad6685.js"
+  }
+]);
